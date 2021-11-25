506c5e44743b73ba7d1ab3d6ca30e1675e70a851
Ali Can Gönüllü <alicangonullu1907@gmail.com>
2021-11-25 19:32:25 +0300

    IPTV FOR PHP
    IPTV FOR PHP

--- libs.php
+++ libs.php
@@ -2,6 +2,20 @@
 
 class IPTVClass {
 
+public function require_auth($AUTH_USER, $AUTH_PASS) {
+	header('Cache-Control: no-cache, must-revalidate, max-age=0');
+	$has_supplied_credentials = !(empty($_SERVER['PHP_AUTH_USER']) && empty($_SERVER['PHP_AUTH_PW']));
+	$is_not_authenticated = (
+		!$has_supplied_credentials ||
+		$_SERVER['PHP_AUTH_USER'] != $AUTH_USER ||
+		$_SERVER['PHP_AUTH_PW']   != $AUTH_PASS
+	);
+	if ($is_not_authenticated) {
+		header('HTTP/1.1 401 Authorization Required');
+		header('WWW-Authenticate: Basic realm="Access denied"');
+		exit;
+	}
+}
 public function extControl($name) {
   if (!extension_loaded(''.strip_tags($name).'')) {
     die('The '.strip_tags($name).' extension is not loaded.');
