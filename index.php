4b412eb43c600dfc5b29861ec27968dd8dced1de
Ali Can Gönüllü <alicangonullu@yahoo.com>
2023-01-13 01:07:49 +0300

    IPTV Class for Education and Camera Servers
    IPTV Class for Education and Camera Servers

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ echo '<head>
 
 //Restream Configs
 $text = "LiveCam-IPTV-Server-v1.2"; //Text on Stream
-$configm3u8 = "-listen 1 -vcodec libx264 -s 1280x1024 -pix_fmt yuv420p -preset ultrafast -r 30 -g 60 -b:v 9500k -acodec aac -ar 44100 -threads 6 -qscale 3 -b:a 712000 -vf drawtext=fontfile=arial.ttf:fontcolor=red:box=1:fontsize=20:text=".htmlentities(strip_tags($text))." -bufsize 9500k";
+$configm3u8 = "-listen 1 -vcodec libx264 -s 1280x1024 -pix_fmt yuv420p -preset ultrafast -r 30 -g 60 -b:v 9500k -acodec aac -ar 44100 -threads 6 -qscale 3 -b:a 712000 -vf drawtext=fontfile=arial.ttf:fontcolor=red:box=1:fontsize=20:text=" . htmlentities(strip_tags($text)) . " -bufsize 9500k";
 $configflv = "-deinterlace -vcodec libx264 -x264opts keyint=8:min-keyint=6:scenecut=2 -s 1280x1024 -pix_fmt yuv420p -preset ultrafast -r 30 -g 60 -b:v 9500k -acodec aac -ar 44100 -threads 6 -qscale 3 -b:a 712000 -vf drawtext=fontfile=arial.ttf:fontcolor=red:box=1:fontsize=30:text=AliIPTV -bufsize 9500k";
 
 //M3U8 Streaming
@@ -32,34 +32,34 @@ $link = "rtmp://localhost:1935";
 $token = "";
 
 
-if(!isset($_GET['git'])) {
-$sayfa = 'index';
-} elseif(empty($_GET['git'])) {
+if (!isset($_GET['git'])) {
+	$sayfa = 'index';
+} elseif (empty($_GET['git'])) {
 
-if(substr($_SERVER['HTTP_ACCEPT_LANGUAGE'],0,2) == "tr") {
-$getir->Error("Sayfa Bulunamadı");
-} else {
-$getir->Error("Page Not Found");
-}
+	if (substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2) == "tr") {
+		$getir->Error("Sayfa Bulunamadı");
+	} else {
+		$getir->Error("Page Not Found");
+	}
 
 } else {
-$sayfa = strip_tags($_GET['git']);
+	$sayfa = strip_tags($_GET['git']);
 }
 
 switch ($sayfa) {
-	
-case 'index':
-$code = "sudo yum -y install --nogpgcheck https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+
+	case 'index':
+		$code = "sudo yum -y install --nogpgcheck https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 sudo yum -y install --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-8.noarch.rpm 
 sudo rpm -ivh https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-8.noarch.rpm
 sudo yum -y install nginx epel-release screen php-fpm php-common php-cli mariadb mariadb-server 
 sudo dnf config-manager --enable powertools
 sudo yum -y install ffmpeg ffmpeg-devel
-chown -R apache:apache ".dirname(__FILE__)."
-chmod 777 -R ".dirname(__FILE__)."
+chown -R apache:apache " . dirname(__FILE__) . "
+chmod 777 -R " . dirname(__FILE__) . "
 setenforce 0 && sed -i 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/sysconfig/selinux
 ";
-echo '<script>
+		echo '<script>
   function timedRefresh(timeoutPeriod) {
 	setTimeout("location.reload(true);",timeoutPeriod);
 	}
@@ -67,7 +67,7 @@ echo '<script>
 </script>
 <b>You have to write this code as root user. If you are in linux.</b></br>
 <b>When you want start stream, you must be edit index.php and add your restream m3u8 server. After that when you had started stream, you ccan find your m3u8 file in m3u folder</b>
-<pre>'.$code.'</pre></br>
+<pre>' . $code . '</pre></br>
 <a target="_blank" href="index.php?git=M3UParser">M3U Parser</a></br>
 <a target="_blank" href="index.php?git=stream-m3u8">Stream M3U8</a></br>
 <a target="_blank" href="index.php?git=stream-ts">Stream TS</a></br>
@@ -80,7 +80,7 @@ echo '<script>
 <a href="index.php?git=stream-stop">Stop Streams</a></br>
 <a target="_blank" href="./m3u">M3U Directory</a></br>
 <a target="_blank" href="./log">Log Directory</a></br>';
-break;
+		break;
 
 	case 'M3UParser':
 		echo '<form action="index.php?git=PM3UParser" method="post">
@@ -88,82 +88,82 @@ break;
 		<input type="text" id="url" name="url"><br><br>
 		<input type="submit" value="Parse">
 	  </form>';
-	break;
+		break;
 
 	case 'PM3UParser':
 		$data = json_decode($getir->M3U_Parser($_POST["url"]), true);
 		foreach ($data["list"]["item"] as $list) {
-			echo 'Name : ' . htmlentities(strip_tags($list["title"])) . '<br> URL : '.htmlentities(strip_tags($list["media_url"])).'<br>';
+			echo 'Name : ' . htmlentities(strip_tags($list["title"])) . '<br> URL : ' . htmlentities(strip_tags($list["media_url"])) . '<br>';
 		}
-	break;
+		break;
 
-case 'stream-m3u8':
-if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-$getir->StartM3U8StreamWin(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configm3u8);
-} else {
-$getir->StartM3U8Stream(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configm3u8);
-}
-break;
+	case 'stream-m3u8':
+		if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+			$getir->StartM3U8StreamWin(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configm3u8);
+		} else {
+			$getir->StartM3U8Stream(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configm3u8);
+		}
+		break;
 
-case 'stream-ts':
-if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-$getir->StartTSStreamWin(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configts);
-} else {
-$getir->StartTSStream(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configts);
-}
-break;
+	case 'stream-ts':
+		if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+			$getir->StartTSStreamWin(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configts);
+		} else {
+			$getir->StartTSStream(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configts);
+		}
+		break;
 
-case 'stream-insta':
-if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-$getir->StartIGTSStreamWin(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configflv, strip_tags($instatk));
-} else {
-$getir->StartIGTSStreamLinux(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configflv, strip_tags($instatk));
-}
-break;
+	case 'stream-insta':
+		if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+			$getir->StartIGTSStreamWin(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configflv, strip_tags($instatk));
+		} else {
+			$getir->StartIGTSStreamLinux(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configflv, strip_tags($instatk));
+		}
+		break;
 
-case 'stream-face':
-if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-$getir->StartFacebookTSStreamWin(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configflv, strip_tags($facebooktk));
-} else {
-$getir->StartFacebookTSStreamLinux(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configflv, strip_tags($facebooktk));
-}
-break;
+	case 'stream-face':
+		if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+			$getir->StartFacebookTSStreamWin(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configflv, strip_tags($facebooktk));
+		} else {
+			$getir->StartFacebookTSStreamLinux(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configflv, strip_tags($facebooktk));
+		}
+		break;
 
-case 'stream-twitch':
-if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-$getir->StartTwitchTSStreamWin(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configflv, strip_tags($twitchtk));
-} else {
-$getir->StartTwitchTSStreamLinux(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configflv, strip_tags($twitchtk));
-}
-break;
+	case 'stream-twitch':
+		if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+			$getir->StartTwitchTSStreamWin(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configflv, strip_tags($twitchtk));
+		} else {
+			$getir->StartTwitchTSStreamLinux(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configflv, strip_tags($twitchtk));
+		}
+		break;
 
-case 'stream-restream':
-if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-$getir->StartRestreamTSStreamWin(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configflv, strip_tags($restreamtk));
-} else {
-$getir->StartRestreamTSStreamLinux(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configflv, strip_tags($restreamtk));
-}
-break;
+	case 'stream-restream':
+		if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+			$getir->StartRestreamTSStreamWin(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configflv, strip_tags($restreamtk));
+		} else {
+			$getir->StartRestreamTSStreamLinux(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configflv, strip_tags($restreamtk));
+		}
+		break;
 
-case 'stream-yt':
-if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-$getir->StartYouTubeTSStreamWin(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configflv, strip_tags($youtubetk));
-} else {
-$getir->StartYouTubeTSStreamLinux(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configflv, strip_tags($youtubetk));
-}
-break;
+	case 'stream-yt':
+		if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+			$getir->StartYouTubeTSStreamWin(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configflv, strip_tags($youtubetk));
+		} else {
+			$getir->StartYouTubeTSStreamLinux(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configflv, strip_tags($youtubetk));
+		}
+		break;
 
-case 'stream-custom':
-if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-$getir->StartCstTSStreamWin(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configflv, strip_tags($link), strip_tags($token));
-} else {
-$getir->StartCstTSStreamLinux(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configflv, strip_tags($link), strip_tags($token));
-}
-break;
+	case 'stream-custom':
+		if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+			$getir->StartCstTSStreamWin(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configflv, strip_tags($link), strip_tags($token));
+		} else {
+			$getir->StartCstTSStreamLinux(strip_tags($name), strip_tags($m3u8), strip_tags($m3u8), $configflv, strip_tags($link), strip_tags($token));
+		}
+		break;
 
-case 'stream-stop':
-$getir->StopFFMPEG();
-break;
-	
+	case 'stream-stop':
+		$getir->StopFFMPEG();
+		break;
+
 }
 ?>
--- libs.php
+++ libs.php
@@ -2,14 +2,15 @@
 class IPTVClass
 {
 
-  public function M3U_Parser($url) {
+  public function M3U_Parser($url)
+  {
     $m3ufile = file_get_contents($url);
     //$re = '/#(EXTINF|EXTM3U):(.+?)[,]\s?(.+?)[\r\n]+?((?:https?|rtmp):\/\/(?:\S*?\.\S*?)(?:[\s)\[\]{};"\'<]|\.\s|$))/';
     $re = '/#EXTINF:(.+?)[,]\s?(.+?)[\r\n]+?((?:https?|rtmp):\/\/(?:\S*?\.\S*?)(?:[\s)\[\]{};"\'<]|\.\s|$))/';
     //$attributes = '/([a-zA-Z0-9\-]+?)="([^"]*)"/';
     $attributes = '/([a-zA-Z0-9\-\_]+?)="([^"]*)"/';
-    
-    
+
+
     $m3ufile = str_replace('tvg-logo', 'thumb_square', $m3ufile);
     $m3ufile = str_replace('tvg-id', 'id', $m3ufile);
     //$m3ufile = str_replace('tvg-name', 'group', $m3ufile);
@@ -18,33 +19,33 @@ class IPTVClass
     $m3ufile = str_replace('group-title', 'group', $m3ufile);
     $m3ufile = str_replace('tvg-country', 'country', $m3ufile);
     $m3ufile = str_replace('tvg-language', 'language', $m3ufile);
-    
+
     //print_r($m3ufile);
-    
+
     //$m3ufile = str_replace(' ', '_', $m3ufile); // FOR GROUP
-    
+
     preg_match_all($re, $m3ufile, $matches);
-    
+
     // Print the entire match result
     //print_r($matches);
-    
+
     $items = array();
-    
-     foreach($matches[0] as $list) {
-        
-         //echo "$list <br>";
-       
-       preg_match($re, $list, $matchList);
-    
-       //$mediaURL = str_replace("\r\n","",$matchList[4]);
-       //$mediaURL = str_replace("\n","",$matchList[4]);
-       //$mediaURL = str_replace("\n","",$mediaURL);
-       $mediaURL = preg_replace("/[\n\r]/","",$matchList[3]);
-       $mediaURL = preg_replace('/\s+/', '', $mediaURL);
-       //$mediaURL = preg_replace( "/\r|\n/", "", $matches[4] );
-       
-    
-       $newdata =  array (
+
+    foreach ($matches[0] as $list) {
+
+      //echo "$list <br>";
+
+      preg_match($re, $list, $matchList);
+
+      //$mediaURL = str_replace("\r\n","",$matchList[4]);
+      //$mediaURL = str_replace("\n","",$matchList[4]);
+      //$mediaURL = str_replace("\n","",$mediaURL);
+      $mediaURL = preg_replace("/[\n\r]/", "", $matchList[3]);
+      $mediaURL = preg_replace('/\s+/', '', $mediaURL);
+      //$mediaURL = preg_replace( "/\r|\n/", "", $matches[4] );
+
+
+      $newdata = array(
         //'ATTRIBUTE' => $matchList[2],
         'service' => "iptv",
         'title' => $matchList[2],
@@ -53,53 +54,54 @@ class IPTVClass
         'playlistURL' => $url,
         'media_url' => $mediaURL,
         'url' => $mediaURL
-        );
-        
-        preg_match_all($attributes, $list, $matches, PREG_SET_ORDER);
-        
-        foreach ($matches as $match) {
-           $newdata[$match[1]] = $match[2];
-        }
-        
-        //array_push($newdata,$attribute);
-        //$newdata[] = $attribute;
-       
-       $items[] = $newdata;
-       //$items[] = $matchList[2];
-        
-     }
-    
+      );
+
+      preg_match_all($attributes, $list, $matches, PREG_SET_ORDER);
+
+      foreach ($matches as $match) {
+        $newdata[$match[1]] = $match[2];
+      }
+
+      //array_push($newdata,$attribute);
+      //$newdata[] = $attribute;
+
+      $items[] = $newdata;
+      //$items[] = $matchList[2];
+
+    }
+
     //   $globalitem =  array (
     //    //'ATTRIBUTE' => $matchList[2],
     //    'item' => $items
     //    );
-    
+
     //$globalitem[$items] ;
     //$globalitems['item'] = $items;
-    
-    //$globalist['list'] = $globalitems;
-    
-       $globalitems =  array (
-        //'ATTRIBUTE' => $matchList[2],
-        'service' => "iptv",
-        'title' => "iptv",
-        'item' => $items,
-        );
-    
-      $globalist['list'] = $globalitems;
-      //print_r($items);
-        return json_encode($globalist);
-    }
 
-  public function RandomString($length = 10) {
+    //$globalist['list'] = $globalitems;
+
+    $globalitems = array(
+      //'ATTRIBUTE' => $matchList[2],
+      'service' => "iptv",
+      'title' => "iptv",
+      'item' => $items,
+    );
+
+    $globalist['list'] = $globalitems;
+    //print_r($items);
+    return json_encode($globalist);
+  }
+
+  public function RandomString($length = 10)
+  {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $charactersLength = strlen($characters);
     $randomString = '';
     for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, $charactersLength - 1)];
+      $randomString .= $characters[rand(0, $charactersLength - 1)];
     }
     return $randomString;
-}
+  }
 
   public function require_auth($AUTH_USER, $AUTH_PASS)
   {
